097e5b7a506dab77f8bc102d765402b604919305
Vinayaksharmagit <138707941+Vinayaksharmagit@users.noreply.github.com>
2023-11-09 09:22:38 +0530

    Update 01-string-replace.py

--- Day-02/examples/01-string-replace.py
+++ Day-02/examples/01-string-replace.py
@@ -1,3 +1,4 @@
-text = "Python is awesome"
-new_text = text.replace("awesome", "great")
-print("Modified text:", new_text)
+###############replace newword
+demo="new game work is that study and job"
+anything=demo.replace("game","real")  ##############"game" is old word and "real" which you want replace with new word
+print("what is your choice ",anything)
