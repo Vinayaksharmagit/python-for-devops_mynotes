383a93de2c9e21a3dee9864b0dd1f8f71d096c2d
Vinayaksharmagit <138707941+Vinayaksharmagit@users.noreply.github.com>
2023-11-09 14:19:30 +0530

    Update 02-float.py

--- Day-02/examples/02-float.py
+++ Day-02/examples/02-float.py
@@ -1,20 +1,23 @@
-# Float variables
-num1 = 5.0
-num2 = 2.5
+#########Floating(decimal)
 
-# Basic Arithmetic
-result1 = num1 + num2
-print("Addition:", result1)
+number1 = 5.6
+number2 = 4.0
 
-result2 = num1 - num2
-print("Subtraction:", result2)
+result1=number1+number2
+print("the value will be",result1)   
 
-result3 = num1 * num2
-print("Multiplication:", result3)
+result2=number1-number2
+print("the value will be",result2)
 
-result4 = num1 / num2
-print("Division:", result4)
+result3=number1*number2
+print("the value will be",result3)
 
-# Rounding
-result5 = round(3.14159265359, 2)  # Rounds to 2 decimal places
-print("Rounded:", result5)
+result4=number1/number2
+print("the value will be",result4)
+
+ 
+
+
+###############round(after decimal how many number you want it will calculate accordingly)
+result5= round(3.6157844,3) #####if you put 3 then it will calculate accordingly  .6157844 to show you in 3 value after decimal
+print("value will be",result5)
